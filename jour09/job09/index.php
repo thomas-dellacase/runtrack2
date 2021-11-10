aa3453432ca40b3c64513399462fc3d5cde5e98b
thomas-dellacase <thomas.dellacase@laplateforme.io>
2021-11-10 14:55:56 +0100

    PHP SQL

---
+++ jour09/job09/index.php
@@ -0,0 +1,55 @@
+<?php
+ $con = new mysqli("localhost","root","root","jour08");
+
+ if($con->connect_errno){
+     echo "Failed" . $con -> connect_error;
+    }
+     $query = $con->prepare("SELECT * FROM (salles) ORDER BY capacite DESC");
+     $query->execute(); 
+
+     
+     $resultSet = $query->get_result();
+     $titres= $resultSet->fetch_assoc();
+     $result = $resultSet->fetch_all();
+
+    //  echo "<pre>";
+    //  print_r($result);
+    //  echo "</pre>";
+
+?>
+
+
+<table>
+
+<thead>
+<?php
+foreach ($titres as $titre => $value) 
+    {
+        echo "<td>" . $titre . "</td>";
+    }
+?>
+</thead>
+<tbody>
+    <tr>
+    <?php
+    foreach ($titres as $titre => $value) {
+        echo "<td>" . $value . "</td>";
+    }
+    ?>
+    </tr>
+    <tr>
+        <?php
+          foreach ($result as $key => $values) 
+          {
+              echo "<tr>";
+              foreach ($values as $key => $value) 
+              {
+                  echo "<td>" . $value . "</td>";
+              }
+              echo "</tr>";
+          }
+        ?>
+    </tr>
+</tbody>
+
+</table>
