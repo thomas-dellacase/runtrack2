50f58b1b75720a263f29b2c47be6f4e59a4c8adc
thomas-dellacase <thomas.dellacase@laplateforme.io>
2021-11-15 11:17:29 +0100

    ajout texte selection

--- jour06/job06/index.php
+++ jour06/job06/index.php
@@ -18,12 +18,13 @@ if(isset($_GET['style'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href=<?php echo $css; ?> type="text/css"
+    <link rel="stylesheet" href=<?php echo $css; ?> type="text/css">
     <title>Document</title>
 </head>
 <body>
+    <?php echo $css ." choisit" ?>
 <form method="GET" action="index.php">
-    <label for="style">String</label>
+    <label for="style"></label>
     <select name="style" id="style">
         <option value="style1" name="style1">style1</option>
         <option value="style2" name="style2">style2</option>
